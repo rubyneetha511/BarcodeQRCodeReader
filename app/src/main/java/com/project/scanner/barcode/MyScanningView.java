14b31bd04cbc5ba7f548319eee39c40d070ca504
rubyneetha511@gmail.com <rubyneetha511@gmail.com>
2017-07-02 13:19:07 -0700

    First real contents

---
+++ app/src/main/java/com/project/scanner/barcode/MyScanningView.java
@@ -0,0 +1,97 @@
+package com.project.scanner.barcode;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyScanningView extends View {
+    private Paint paint = new Paint();
+    private int mPosY = 0;
+    private boolean runAnimation = true;
+    private boolean showLine = true;
+    private Handler handler;
+    private Runnable refreshRunnable;
+    private boolean isGoingDown = true;
+    private int mHeight;
+
+    public MyScanningView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyScanningView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyScanningView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint.setColor(Color.WHITE);
+        //Add anything else you want to customize your line, like the stroke width
+        handler = new Handler();
+        refreshRunnable = new Runnable() {
+            @Override
+            public void run() {
+                refreshView();
+            }
+        };
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        mHeight = canvas.getHeight();
+        if (showLine) {
+            canvas.drawLine(0, mPosY, canvas.getWidth(), mPosY, paint);
+        }
+        if (runAnimation) {
+            handler.postDelayed(refreshRunnable, 0);
+        }
+    }
+
+    public void startAnimation() {
+        runAnimation = true;
+        showLine = true;
+        this.invalidate();
+    }
+
+    public void stopAnimation() {
+        runAnimation = false;
+        showLine = false;
+        reset();
+        this.invalidate();
+    }
+
+    private void reset() {
+        mPosY = 0;
+        isGoingDown = true;
+    }
+
+    private void refreshView() {
+        //Update new position of the line
+        if (isGoingDown) {
+            mPosY += 5;
+            if (mPosY > mHeight) {
+                //We invert the direction of the animation
+                mPosY = mHeight;
+                isGoingDown = false;
+            } else {
+                mPosY -= 5;
+                if (mPosY < 0) {
+                    //We invert the direction of the animation
+                    mPosY = 0;
+                    isGoingDown = true;
+                }
+                this.invalidate();
+            }
+
+        }
+    }
+}
